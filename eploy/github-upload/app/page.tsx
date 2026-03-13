8dec859da3b640aed64890ec9fbe0f885576fafb
ShaneSilk <765541782@qq.com>
2026-03-13 15:18:00 +0800

    chore: upload project files

---
+++ eploy/github-upload/app/page.tsx
@@ -0,0 +1,9 @@
+﻿import { ResumeWorkspace } from "@/components/resume-builder/ResumeWorkspace";
+
+export default function HomePage() {
+  return (
+    <main className="print-root min-h-screen p-4 lg:p-6">
+      <ResumeWorkspace />
+    </main>
+  );
+}
